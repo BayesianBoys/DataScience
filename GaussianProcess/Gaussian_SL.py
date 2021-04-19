76542f8824de9ca4d797fdf17d521df2413abaf7
gotticketsto360tour <werling1407@gmail.com>
2021-04-19 19:38:31 +0200

    setting default values

--- GaussianProcess/Gaussian_SL.py
+++ GaussianProcess/Gaussian_SL.py
@@ -29,13 +29,13 @@ st.image("bayes_bois.png", width = 100)
 Let's simulate data!
 '''
 
-lin_trend = st.slider("Linear trend", -50.0, 50.0)
-poly_trend = st.slider("Polynomial Trend", -1.0, 1.0)
-sinus = st.slider("Sinus", 0.0, 100.0)
-sinus_2 = st.slider("Sinus 2", 0.0, 100.0)
-sinus_2_period = st.slider("Sinus 2 Period", 0.0, 100.0)
+lin_trend = st.slider("Linear trend", -50.0, 50.0, 0.4)
+poly_trend = st.slider("Polynomial Trend", -1.0, 1.0, 0.0)
+sinus = st.slider("Sinus", 0.0, 100.0, 1.0)
+sinus_2 = st.slider("Sinus 2", 0.0, 100.0, 1.0)
+sinus_2_period = st.slider("Sinus 2 Period", 0.0, 100.0, 0.4)
 
-noise_amount = st.slider("Noise Amount", 1.0, 10.0, 1.0)
+noise_amount = st.slider("Noise Amount", 1.0, 100.0, 1.0)
 
 ## FUNCTION 
 def f(x):
